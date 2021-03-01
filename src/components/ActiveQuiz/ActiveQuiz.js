6da75aeb370b47d9bff4b5f98f6a047993bd2eca
mexell96 <mexell96@gmail.com>
2021-03-01 11:46:33 +0300

    added ActiveQuiz

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,26 @@
+import classes from "./ActiveQuiz.module.css";
+
+import React from "react";
+
+const ActiveQuiz = () => {
+  return (
+    <div className={classes.ActiveQuiz}>
+      <p className={classes.Question}>
+        <span>
+          <strong>1.</strong>&nbsp; Как дела?
+        </span>
+
+        <small>4 из 12</small>
+      </p>
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ActiveQuiz;
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import classes from "./Quiz.module.css";
 
 class Quiz extends Component {
   state = {
-    quiz: []
-  }
+    quiz: [],
+  };
   render() {
-    return <div className={classes.Quiz}>
-      <h1>Quiz</h1>
-    </div>;
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Quiz</h1>
+          <ActiveQuiz />
+        </div>
+      </div>
+    );
   }
 }
 
